3fa7bc880b58f3196e32a52b209b16a187500913
Koen Bolhuis <koen.bolhuis@gmail.com>
2022-11-29 12:46:16 +0100

    Update ShareholderVoting

--- assignment-2/ShareholderVoting.sol
+++ assignment-2/ShareholderVoting.sol
@@ -5,7 +5,41 @@ pragma solidity >=0.7.0 <0.9.0;
 contract ShareholderVoting {
     address private director;
 
+    mapping(address => bool) private shareholders;
+    mapping(uint => address[]) private voters;
+
+    struct Question {
+        string text;
+        uint votesFor;
+        uint votesAgainst;
+        bool closed;
+    }
+    Question[] private questions;
+
+    modifier onlyDirector() {
+        require(msg.sender == director);
+        _;
+    }
+
     constructor() {
         director = msg.sender;
+    }
+
+    function addShareholder(address shareholder) public onlyDirector {
+        shareholders[shareholder] = true;
+    }
+
+    function removeShareholder(address shareholder) public onlyDirector {
+        shareholders[shareholder] = false;
+    }
+
+    function addQuestion(string calldata text) public onlyDirector returns (uint id) {
+        questions.push(Question(text, 0, 0, false));
+        uint questionID = questions.length - 1;
+        return questionID;
+    }
+
+    function closeQuestion(uint id) public onlyDirector {
+        questions[id].closed = true;
     }
 }
